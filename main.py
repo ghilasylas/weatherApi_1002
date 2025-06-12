5867d93e58e7dbb7766b91fed4d5b7fcf02649ae
ghilasylas <ghilas.amri1@gmail.com>
2025-06-12 15:00:25 -0400

    update

---
+++ main.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+from fastapi import FastAPI
+from dotenv import load_dotenv
+import requests
+import uvicorn
+
+load_dotenv()
+app = FastAPI()
+
+city = "London"
+api_key = "20ba69e7192d543f70ce7b23aea01e0a"
+
+
+def get_weather():
+    url=f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+    try:
+        response = requests.get(url)
+        data = response.json()
+        weather= {
+            "city": data["name"],
+            "temperature": data["main"]["temp"],
+            "description": data["weather"][0]["description"],
+            "icon": data["weather"][0]["icon"],
+            "timestamp": datetime.now().isoformat()
+        }
+    except Exception as e:
+
+        weather = {
+            'city':'unknown',
+            'temperature': 'n/a',
+            'description': 'enable to go data'
+        }
+    return weather
+
+
+@app.get("/info")
+async def get_info():
+    current_datetime = datetime.now()
+    formatted_date = current_datetime.strftime("%Y-%m-%d")
+    formatted_time = current_datetime.strftime("%H:%M:%S")
+
+    weather= get_weather()
+
+    return {
+        "date": formatted_date,
+        "time": formatted_time,
+        "weather": weather
+    }
